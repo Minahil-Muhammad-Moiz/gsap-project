2600d62affac8d3725c509e98b3e596f7ebabe48
Your Name <minahil4864@gmail.com>
2024-08-24 13:57:47 +0500

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+import * as THREE from 'three';
+
+const renderer = new THREE.WebGLRenderer();
+//  Rendere is a tool that three JS uses to allocate a space on a web page where we can add and animate a 3D stuff.
+
