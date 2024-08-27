079464b464bb90e423fb34a909a12d4f62a65bbf
Your Name <minahil4864@gmail.com>
2024-08-27 15:07:30 +0500

    modified basic code

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+// renderer is the tool Three.js uses to allocate space on a webpage where we can display and manipulate 3D content.
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const scene = new THREE.Scene();
+// scene is the container for all the objects in our 3D world.
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 camera.position.z = 5;
 
-function animate() {
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+// cube is the object we're adding to the scene.
 
+function animate() {
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
-
+	renderer.render(scene, camera);
 }
+
+animate();
