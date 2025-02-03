1e27a748df6e1bf53d8b1d3a2b7c41665fe90cda
Minahil-Muhammad-Moiz <minahil4864@gmail.com>
2025-02-03 19:47:51 +0500

    cursor effect

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,24 @@ import SVGanimation from './components/SVGanimation';
 
 const App = () => {
 
+  const handleCursor = (dets) => {
+    gsap.to('#cursor', {
+      x: dets.clientX,
+      y: dets.clientY,
+      duration: 1,
+      ease: "back.out",
+    })
+  }
+
   return (
-    <>
+    <div onMouseMove={handleCursor}>
+      <div id='cursor' className='z-40 w-4 h-4 rounded-full fixed bg-amber-400'></div>
       <Header />
       <main className='h-screen w-full text-white'>
         <SVGanimation />
-        <HorizontalScroll />
       </main>
-    </>
+      <HorizontalScroll />
+    </div>
   )
 }
 
