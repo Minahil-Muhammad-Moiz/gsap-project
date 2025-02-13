b74e2c388a1b2e983943e2d8232aa3e9561cc04d
Minahil-Muhammad-Moiz <minahil4864@gmail.com>
2025-02-13 23:34:53 +0500

    loader animation

--- src/App.jsx
+++ src/App.jsx
@@ -12,24 +12,34 @@ import Marquee from './components/Marquee';
 const App = () => {
   const [cursorText, setCursorText] = useState('')
 
-  const handleCursor = (dets) => {
-    gsap.to('#cursor', {
-      x: dets.clientX,
-      y: dets.clientY,
-      duration: 1,
-      ease: "back.out",
+  const tl = gsap.timeline()
+  useGSAP(() => {
+    tl.to('#loader .moveUp', {
+      height: 0,
+      delay: 0.9,
+      stagger:0.3
     })
-  }
+    tl.to('#loader', {
+      height: 0,
+      delay: -0.1
+    })
+  })
 
   return (
-    <div className='h-full w-full'>
-      <div id='page1' className='h-screen w-full text-black  bg-[#F5E31A] flex justify-center items-center font-bold'>
-        <h1><em>We are a</em> CREATIVE <em>studio</em> DEDICATED <em>to</em> CULTURAL
-          ADVANCEMENT <em>through</em> STRATEGY <em>and</em> DESIGN.</h1>
+    <>
+      <div id='loader' className='h-screen w-full text-white bg-[#3b3b3b] fixed text-center'>
+        <div className='moveUp bg-[#F5E31A]  w-full h-full'></div>
+        <div className='moveUp bg-[#d900ff]  w-full h-full'></div>
       </div>
-      <div id='page2' className='bg-black w-full h-screen'></div>
-      <div id='page3' className='bg-white w-full h-screen'></div>
-    </div>
+      <div className='h-full w-full' id='main'>
+        <div id='page1' className='h-screen w-full text-[#555555]  bg-[#F5E31A] flex justify-center items-center text-center'>
+          <h1 className='text-[6.9vw] tracking-[-2px] leading-[5.3vw]'><em>We are a</em> CREATIVE <em>studio</em><br /> DEDICATED <em>to</em> CULTURAL<br />
+            ADVANCEMENT <em>through</em><br /> STRATEGY <em>and</em> DESIGN.</h1>
+        </div>
+        <div id='page2' className='bg-black w-full h-screen'></div>
+        <div id='page3' className='bg-white w-full h-screen'></div>
+      </div>
+    </>
   )
 }
 
